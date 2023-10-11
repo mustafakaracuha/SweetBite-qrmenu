3b8152c7c17979c94ec6fda8dcc54ce51adbc969
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-10-11 16:50:26 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import QRScanner from "./pages/Scanner/QRScanner";
 import QRMenu from "./pages/Menu/QRMenu";
 import "./index.css";
