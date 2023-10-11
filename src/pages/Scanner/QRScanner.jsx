e610045be6790a8cccd804331d9d4b793a89e2fd
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-10-11 11:33:25 +0300

    update helmet

--- src/pages/Scanner/QRScanner.jsx
+++ src/pages/Scanner/QRScanner.jsx
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import QRCode from "react-qr-code";
+import { Helmet } from "react-helmet";
 
 function QRScanner() {
   const [value, setValue] = useState("https://piloveyou-menu.vercel.app/menu");
 
   return (
     <div className=" max-w-md mx-auto flex flex-col items-center justify-center min-h-screen">
+       <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pi LOVE YOU</title>
+      </Helmet>
       <h1 className="text-5xl font-extrabold mb-2 text-rose-500">Pi LOVE YOU</h1>
       <h2 className="text-3xl font-bold mb-8 text-white">Hoş geldiniz</h2>
 
