69bed5274120d7f367bf0138764e5c4b306046f4
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-10-12 17:12:20 +0300

    update

--- src/pages/Menu/QRMenu.jsx
+++ src/pages/Menu/QRMenu.jsx
@@ -80,23 +80,23 @@ const QrMenu = () => {
             }`}
           >
             <div className="w-full h-[4px] mb-2 rounded-full bg-rose-500"></div>
-            <ul className="bg-transparent">
+            <ul className="bg-transparent h-auto overflow-hidden">
               {menuItems
                 .filter((menuItem) => menuItem.category === category)
                 .map((menuItem) => (
                   <li
                     key={menuItem.id}
-                    className="py-2 flex items-center justify-between"
+                    className="py-4 px-4 mt-2 mb-3 bg-[#1a1a1a] rounded-xl p-2 flex items-center justify-between"
                   >
-                    <div className="flex flex-col items-start">
-                      <strong className="text-lg font-normal text-white pl-2">
+                    <div className="flex flex-col items-start bg-transparent">
+                      <strong className="text-lg font-normal bg-transparent text-white pl-2">
                         {menuItem.name}
                       </strong>
-                      <p className="text-md text-gray-500 pl-2">
+                      <p className="text-md text-gray-500 pl-2 bg-transparent">
                         {menuItem.description}
                       </p>
                     </div>
-                    <div className="flex items-center justify-between mt-2">
+                    <div className="flex items-center justify-between mt-2 bg-transparent">
                       <p className="w-14 flex items-center justify-center text-md text-black bg-white p-2 rounded-xl font-semibold">
                         {menuItem.price} ₺
                       </p>
