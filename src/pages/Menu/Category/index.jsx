d90d620b95dc8a9e7b730235742a05a050f5ab8d
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-06-17 20:07:15 +0300

    update icons

--- src/pages/Menu/Category/index.jsx
+++ src/pages/Menu/Category/index.jsx
@@ -5,8 +5,14 @@ import ListItem from "../ListItem";
 import fork from "../../../fork.svg";
 import { FaSpoon } from "react-icons/fa6";
 
+import hookahIcon from "../../../assets/app/hookah.png";
+import hotDrinkIcon from "../../../assets/app/hotDrinks.png";
+import coldDrinkIcon from "../../../assets/app/coldDrinks.png";
+import dessertIcon from "../../../assets/app/dessert.png";
+import iceCreamIcon from "../../../assets/app/iceCream.png";
+
 function index() {
-    const [initialCategory] = useState("NARGİLE");
+    const [initialCategory] = useState("");
     const categories = ["NARGİLE", "SICAK İÇECEKLER", "SOĞUK İÇECEKLER", "TATLI", "DONDURMA"];
 
     const [categoryVisibility, setCategoryVisibility] = useState({});
@@ -31,7 +37,25 @@ function index() {
                 <div key={category} className="mb-4">
                     <h2 onClick={() => toggleCategoryVisibility(category)} className="text-2xl flex items-center justify-between mb-3 zIndex text-white font-bold rounded-md pl-2 cursor-pointer">
                         {category}
-                        <p>{!categoryVisibility[category] ? <FaSpoon size={23} className="text-pink-500 mr-1" /> : <img src={fork} className=" rotate-[41deg] mr-3" width={8} alt="" />}</p>
+                        <p>
+                            {!categoryVisibility[category] ? (
+                                category === "NARGİLE" ? (
+                                    <img src={hookahIcon} className="mr-1" width={25} alt="Nargile Icon" />
+                                ) : category === "SICAK İÇECEKLER" ? (
+                                    <img src={hotDrinkIcon} className="mr-1" width={22} alt="Hot Drink Icon" />
+                                ) : category === "SOĞUK İÇECEKLER" ? (
+                                    <img src={coldDrinkIcon} className="mr-1" width={26} alt="Cold Drink Icon" />
+                                ) : category === "TATLI" ? (
+                                    <img src={dessertIcon} className="mr-1" width={25} alt="Dessert Icon" />
+                                ) : category === "DONDURMA" ? (
+                                    <img src={iceCreamIcon} className="mr-1" width={28} alt="Ice Cream Icon" />
+                                ) : (
+                                    ""
+                                )
+                            ) : (
+                                ""
+                            )}
+                        </p>
                     </h2>
                     <div className={`category-content ${!categoryVisibility[category] ? "" : "collapsed"}`}>
                         <div className="w-full h-[4px] mb-2 rounded-full bg-pink-500"></div>
