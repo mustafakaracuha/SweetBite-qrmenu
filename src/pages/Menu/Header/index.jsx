bf6c7dbbe2ae50cfb9e7a2f8cdecb0a80c41bc2a
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-06-17 20:50:27 +0300

    update begonya logo

--- src/pages/Menu/Header/index.jsx
+++ src/pages/Menu/Header/index.jsx
@@ -6,7 +6,7 @@ function index() {
         <>
             <div className="flex items-center">
                 <h1 className="text-4xl font-serif mb-2 text-pink-500">Begonya</h1>
-                <GiFlowerEmblem className="text-pink-500 ml-4 animate-spin" size={40} />
+                <GiFlowerEmblem className="text-pink-500 ml-3 animate-spin" size={40} />
             </div>
             <h1 className="text-4xl font-semibold mb-8 text-start text-white pl-2">Cafe & Lounge</h1>
         </>
